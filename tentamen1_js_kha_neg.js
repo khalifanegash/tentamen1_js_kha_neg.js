c429ca997d44c5863d2a21e6c312b35f2cfdda5d
Khalifa Negash <khalifanegash@outlook.com>
2019-11-28 16:24:53 +0100

    Add files via upload

---
+++ tentamen1_js_kha_neg.js
@@ -0,0 +1,72 @@
+var groceries = ['mjöl', 'potatis', 'äpple',  'ägg'];
+if (i = groceries) {
+     console.log(groceries);
+     
+}
+var prices = [220, 55, 99, 123];
+     console.log(prices);
+    
+function shop(prices =[220, 55, 99, 123]){
+if (prics < 500){
+     return(true);
+     
+}
+else{
+     return(false);
+}
+};  
+  var canAfford; 
+  function myFunction() {
+     var x = document.getElementById("my-button").value;
+     document.getElementById("demo").innerHTML = x;
+     document.background-color('blue');
+   }
+   var boat = {color: 'green', speed: 75}
+    function drive() {
+        console.log(boat.color);
+        
+   }
+   drive();
+   // AJAX  'Asynchronous JavaScript And XML. 
+   // AJAX is not a programming language.
+   var val3 = 10;
+
+function calc(val1, val2){
+  
+  
+  let sum = val1 + val2 + val3;
+
+  var val3 = 35;
+
+  for(let i = 0; i < 3; i++){
+    val3 += i;
+  }
+
+  return sum + val3;
+}
+
+let result = calc(10, 23);
+   /**
+    * val3 värd med 10
+    * function calc har val1 och val2 
+    * sum variable har val1 + val2 +val3
+    * val3 har två värd inne i function = 35 och ute = 10
+    */
+   
+
+    /**
+     * jQuery is a lightweight, "write less, do more", JavaScript library.
+     * jQuery is to make  much easier to use JavaScript on your website.
+     * the first thing is you must have know a javascript atherwase you dont anderstand it.
+     * the negative thing is been a difficalt to anderstand it with out javascript before.
+     */
+    //$('button').addClass('primary-button') 
+    // this do a button by jquery and add a class och the button are type primary.
+
+    /**
+     * u11
+     * The innerHTML property sets or returns the HTML content (inner HTML) of an element.
+     * you can create a <p> element with some text and append it to the end of the document body:
+
+
+     */
